ec49e8b15fe6c3aac082f2ebc0b66c15dc8d30ab
Rajatm544 <rajatm544@gmail.com>
2020-04-11 12:05:35 +0530

    change server package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 //config Express App
