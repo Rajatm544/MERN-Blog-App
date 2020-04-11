b2a783e73523177b600e02743e602646e61bf694
Rajatm544 <rajatm544@gmail.com>
2020-04-11 16:21:32 +0530

    change path

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
+            path.resolve(__dirname, "../frontend", "build", "index.html")
         );
     });
 }
