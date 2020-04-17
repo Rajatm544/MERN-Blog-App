3b5444da68c07a8d78ab89ea17805c77d16a8476
Rajatm544 <rajatm544@gmail.com>
2020-04-17 11:53:11 +0530

    catch error in login auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,24 +6,26 @@ router.route("/login").post((req, res) => {
     const { username, socialId } = req.body;
 
     // Find or create a new user and send it as response
-    User.findOne({ socialId: socialId }).then((foundUser) => {
-        if (foundUser) {
-            res.json(foundUser);
-        } else {
-            const newUser = new User({
-                username: username,
-                socialId: socialId,
-            });
-            newUser
-                .save()
-                .then(() =>
-                    User.findOne({ socialId: socialId }).then((foundNewUser) =>
-                        res.json(foundNewUser)
+    User.findOne({ socialId: socialId })
+        .then((foundUser) => {
+            if (foundUser) {
+                res.json(foundUser);
+            } else {
+                const newUser = new User({
+                    username: username,
+                    socialId: socialId,
+                });
+                newUser
+                    .save()
+                    .then(() =>
+                        User.findOne({
+                            socialId: socialId,
+                        }).then((foundNewUser) => res.json(foundNewUser))
                     )
-                )
-                .catch((err) => res.status(400).json("Error: " + err));
-        }
-    });
+                    .catch((err) => res.status(400).json("Error: " + err));
+            }
+        })
+        .catch((err) => res.status(400).json("Error : " + err));
 });
 
 module.exports = router;
