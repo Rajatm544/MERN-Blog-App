58e6ec5208d4398e22abca01e64a3bcec1ec5aca
Rajatm544 <rajatm544@gmail.com>
2020-06-16 19:50:47 +0530

    remove unwanted comments

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-// app.use(cors({Acces - Control - Allow - Origin: *}));
 
 //config PORT
 const PORT = process.env.PORT || 5000;
