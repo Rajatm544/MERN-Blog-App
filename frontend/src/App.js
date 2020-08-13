ae47a20951e29b885f9c2cdfee3795f46065fb64
Rajatm544 <rajatm544@gmail.com>
2020-08-13 19:00:28 +0530

    lazy load all the imported components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Import style
 import "./stylesheets/index.css";
 
 //Import all components
-import Navbar from "./components/Navbar";
-import Landing from "./components/Landing";
-import Post from "./components/Post";
-import CreatePost from "./components/CreatePosts";
-import EditPost from "./components/EditPost";
-import PostsList from "./components/PostsList";
-import Login from "./components/Login";
-import About from "./components/About";
-import Footer from "./components/Footer";
+const Navbar = lazy(() => import("./components/Navbar"));
+const Landing = lazy(() => import("./components/Landing"));
+const Post = lazy(() => import("./components/Post"));
+const CreatePost = lazy(() => import("./components/CreatePosts"));
+const EditPost = lazy(() => import("./components/EditPost"));
+const PostsList = lazy(() => import("./components/PostsList"));
+const Login = lazy(() => import("./components/Login"));
+const About = lazy(() => import("./components/About"));
+const Footer = lazy(() => import("./components/Footer"));
+
+const renderLoader = () => (
+    <div className="spinner-container">
+        <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    </div>
+);
 
 const App = () => (
     <div className="container">
         <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Landing} />
-                <Route path="/posts" exact component={PostsList} />
-                <Route path="/posts/new/" exact component={CreatePost} />
-                <Route path="/posts/:id" exact component={Post} />
-                <Route path="/posts/:id/edit" exact component={EditPost} />
-                <Route path="/login" component={Login} />
-                <Route path="/about" component={About} />
-            </Switch>
-            <Footer />
+            <Suspense fallback={renderLoader()}>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Landing} />
+                    <Route path="/posts" exact component={PostsList} />
+                    <Route path="/posts/new/" exact component={CreatePost} />
+                    <Route path="/posts/:id" exact component={Post} />
+                    <Route path="/posts/:id/edit" exact component={EditPost} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/about" component={About} />
+                </Switch>
+                <Footer />
+            </Suspense>
         </Router>
     </div>
 );
